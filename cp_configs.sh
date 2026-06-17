88753251e5d1e39985f163448ef2b440bc513efb
cpa <cpa@bbqtron>
2026-06-17 14:13:53 -0600

    fixes on cpconfigs.sh

--- cp_configs.sh
+++ cp_configs.sh
@@ -4,7 +4,6 @@
 cp ./rofidmenu.rasi /home/$USER/.config/rofi/rofidmenu.rasi
 cp ./config.rasi /home/$USER/.config/rofi/config.rasi
 cp ./binds.kdl /home/$USER/.config/niri/binds.kdl
-sed -i 's/dms\/binds/binds/' /home/$USER/.config/niri/config.kdl
-sed -i 's/\/\/ focus-follows/focus-follows/' /home/$USER/.config/niri/config.kdl
+cp ./config.kdl /home/$USER/.config/niri/config.kdl
 cp ./alacritty.toml /home/$USER/.config/alacritty/alacritty.toml
 grep -q "QT_QPA_PLATFORMTHEME=qt6ct" /etc/environment || echo "QT_QPA_PLATFORMTHEME=qt6ct" | sudo tee -a /etc/environment >/dev/null
